04d8dd73bbf2f84127fff07f0d2c433b52e820fa
edometro <edometro.isorikakko@gmail.com>
2025-09-28 21:40:09 +0900

    gitignore追加できていなかったぽい

--- pico_logger.ps1
+++ pico_logger.ps1
@@ -1,4 +1,4 @@
 while ($true) {
-  curl http://192.168.4.1/usb | Tee-Object -FilePath output.txt -Append
+  curl http://192.168.40.1/usb | Tee-Object -FilePath output.txt -Append
   Start-Sleep -Milliseconds 50
 }
